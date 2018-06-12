4c84421f8b36bc889c6abb87ce7cae590f267252
Aydin Mir Mohammadi <am@bluehands.de>
2018-06-12 16:23:27 +0200

    Add code

---
+++ TableStorageSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TableStorageSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=dhtablestoragesample;AccountKey=62RsWXw5raAo6IuudBH3NBM34SwVHWzRPzaf+oTmC9V1VWExlxBXc1/Md7y/W/GkJYNs1zfXVVJxNaIqDZc+9g==;EndpointSuffix=core.windows.net";
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            var table = tableClient.GetTableReference("customers");
+            table.CreateIfNotExists();
+
+            var amm = new Customer {RowKey = "amm",PartitionKey="Karlsruhe", Name = "Aydin Mir Mohammadi", Street = "Hebelstrasse 15", ZipCode = "76133", City = "Karlsruhe"};
+
+            table.Execute(TableOperation.Insert(amm));
+            amm.Street = "Hebelstraße 15";
+            table.Execute(TableOperation.Replace(amm));
+
+            var q = from c in table.CreateQuery<Customer>() where c.City.Equals("Karlsruhe", StringComparison.InvariantCultureIgnoreCase) select c;
+            var customer = q.FirstOrDefault();
+        }
+    }
+
+    public class Customer : TableEntity
+    {
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+    }
+}
